4e92d79dec825b280d50830723b46764b7152286
Anthony Fu <anthonyfu117@hotmail.com>
2022-03-25 23:51:03 +0800

    chore: fix

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,7 @@ import { unctxPlugin } from './src/plugin'
 export default defineConfig({
   plugins: [
     unctxPlugin.vite({
+      asyncFunctions: ['withAsyncContext', 'callAsync'],
       helperModule: resolve('./src/index.ts'),
       transformInclude: id => id.includes('async.test')
     })
