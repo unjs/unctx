b54dbd3134806556b1e94c9b4c27f19b2df5a7df
Anthony Fu <anthonyfu117@hotmail.com>
2022-04-04 19:40:13 +0800

    feat!: strict `ctx.use` and new `ctx.tryUse` (#7)

--- test/index.test.ts
+++ test/index.test.ts
@@ -4,7 +4,9 @@ import { createContext, createNamespace } from '../src'
 describe('createContext', () => {
   it('call and use', () => {
     const ctx = createContext()
-    expect(ctx.use()).toBe(null)
+    expect(() => ctx.use()).toThrowError()
+    expect(ctx.tryUse()).toBe(null)
+
     const res = ctx.call('A', () => {
       expect(ctx.use()).toBe('A')
       expect(ctx.use()).toBe('A')
@@ -15,7 +17,7 @@ describe('createContext', () => {
 
   it('context conflict', () => {
     const ctx = createContext()
-    expect(ctx.use()).toBe(null)
+    expect(ctx.tryUse()).toBe(null)
     ctx.call('A', () => {
       expect(() => ctx.call('B', vi.fn())).toThrow('Context conflict')
     })
@@ -30,11 +32,11 @@ describe('createContext', () => {
 
   it('use async', async () => {
     const ctx = createContext()
-    expect(ctx.use()).toBe(null)
+    expect(ctx.tryUse()).toBe(null)
     const res = await ctx.call('A', async () => {
       expect(ctx.use()).toBe('A')
       await Promise.resolve()
-      expect(ctx.use()).toBe(null)
+      expect(ctx.tryUse()).toBe(null)
       return 'OK'
     })
     expect(res).toBe('OK')
@@ -51,13 +53,13 @@ describe('namespace', () => {
 describe('singleton', () => {
   it('set/unset', () => {
     const ctx = createContext()
-    expect(ctx.use()).toBe(null)
+    expect(ctx.tryUse()).toBe(null)
     ctx.set('A')
     ctx.set('A')
     expect(ctx.use()).toBe('A')
     expect(ctx.use()).toBe('A')
     ctx.unset()
-    expect(ctx.use()).toBe(null)
+    expect(ctx.tryUse()).toBe(null)
   })
 
   it('call compatibility', () => {
