6b7b160b557c020287b3c76ea28c9a149c276266
Pooya Parsa <pooya@pi0.io>
2023-04-10 19:56:59 +0200

    update test

--- test/transform.test.ts
+++ test/transform.test.ts
@@ -4,7 +4,7 @@ import { createTransformer } from "../src/transform";
 describe("transforms", () => {
   const transformer = createTransformer({
     asyncFunctions: ["withAsyncContext", "callAsync"],
-    asyncObjectKeys: {
+    objectDefinitions: {
       defineSomething: ["someKey"],
     },
   });
