179fb9b70b5a63d289138520862aa9eea9849f09
Daniel Roe <daniel@roe.dev>
2023-05-22 20:57:06 +0100

    fix: remove intermediate content

--- src/transform.ts
+++ src/transform.ts
@@ -176,8 +176,11 @@ export function createTransformer(options: TransformerOptions = {}) {
     function injectForNode(node: AwaitExpression, parent: Node | undefined) {
       const isStatement = parent?.type === "ExpressionStatement";
 
+      s.remove(toIndex(node.loc.start), toIndex(node.argument.loc.start))
+      s.remove(toIndex(node.loc.end), toIndex(node.argument.loc.end))
+
       s.appendLeft(
-        toIndex(node.loc.start),
+        toIndex(node.argument.loc.start),
         isStatement
           ? `;(([__temp,__restore]=__executeAsync(()=>`
           : `(([__temp,__restore]=__executeAsync(()=>`
