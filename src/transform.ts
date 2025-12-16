6a00736fdc5de3ec96efbe81cca4786db7cd22d6
Daniel Roe <daniel@roe.dev>
2025-12-16 20:12:33 +0000

    perf(plugin): transform hook filter (#122)

--- src/transform.ts
+++ src/transform.ts
@@ -60,6 +60,10 @@ export function createTransformer(options: TransformerOptions = {}) {
     return typeof code === "string" && matchRE.test(code);
   }
 
+  const filter = {
+    code: matchRE,
+  };
+
   function transform(code: string, options_: { force?: false } = {}) {
     if (!options_.force && !shouldTransform(code)) {
       return;
@@ -221,6 +225,7 @@ export function createTransformer(options: TransformerOptions = {}) {
 
   return {
     transform,
+    filter,
     shouldTransform,
   };
 }
